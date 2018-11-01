9d7fd7164ba8b2efcd5c1d0178f38dc2f30310f1
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-11-01 22:47:16 +0545

    Updates

---
+++ precache-manifest.6cb16f10012d58eef601c8ee4a36c314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "340021ed26bcde992272",
+    "url": "/hackernews-ui/static/css/main.6ca8dc58.chunk.css"
+  },
+  {
+    "revision": "340021ed26bcde992272",
+    "url": "/hackernews-ui/static/js/main.340021ed.chunk.js"
+  },
+  {
+    "revision": "008df343f49ca5f7de4f",
+    "url": "/hackernews-ui/static/js/1.008df343.chunk.js"
+  },
+  {
+    "revision": "ff2ef48bbb115788e46a",
+    "url": "/hackernews-ui/static/js/runtime~main.ff2ef48b.js"
+  },
+  {
+    "revision": "aa7f19fa185e639c65bca99b2825f944",
+    "url": "/hackernews-ui/index.html"
+  }
+];
